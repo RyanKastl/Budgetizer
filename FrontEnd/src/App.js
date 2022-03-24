b22eb216bca60219dcfc8b8ba5c1250379dcd94a
BuildTools <unconfigured@null.spigotmc.org>
2022-03-23 22:09:00 -0500

    Split out chart constants

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,51 +1,8 @@
 import './App.css';
+import * as Constants from './Constants/ChartDefaults'
 import { Pie } from 'react-chartjs-2';
 import React, { useState } from 'react';
 
-const backgrounds = [
-  'rgba(255, 99, 132, 0.2)',
-  'rgba(54, 162, 235, 0.2)',
-  'rgba(255, 206, 86, 0.2)',
-  'rgba(75, 192, 192, 0.2)',
-  'rgba(153, 102, 255, 0.2)',
-  'rgba(255, 159, 64, 0.2)',
-];
-
-const borders = [
-  'rgba(255, 99, 132, 1)',
-  'rgba(54, 162, 235, 1)',
-  'rgba(255, 206, 86, 1)',
-  'rgba(75, 192, 192, 1)',
-  'rgba(153, 102, 255, 1)',
-  'rgba(255, 159, 64, 1)',
-];
-
-const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-  datasets: [
-    {
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
 
 function PieChart(props) {
   return (
@@ -134,8 +91,8 @@ class Data extends React.Component {
         {
           label: 'IDK',
           data: amounts,
-          backgroundColor: backgrounds,
-          borderColor: borders,
+          backgroundColor: Constants.ChartDefaults.backgrounds,
+          borderColor: Constants.ChartDefaults.borders,
           borderWidth: 1,
         },
       ],
